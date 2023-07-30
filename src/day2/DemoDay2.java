db114a7b2d1325740bdd61f47c2f0bc5e25c9727
teresa <teresa@teresas-air.lan>
2023-07-30 07:47:12 -0400

    implemeted add()

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,5 +5,13 @@ public class DemoDay2 {
 	public static void main (String args []) {
 		System.out.println("Test");
 	}
+	
+	//add method
+	public static void add( int a, int b) {
+		
+		System.out.println(a+b);
+		
+		
+	}
 
 }
