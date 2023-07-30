2cd96140209ba5f09349622d83a5944e12d2a121
tcrodriguez88 <140897819+tcrodriguez88@users.noreply.github.com>
2023-07-30 08:21:52 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -3,7 +3,10 @@ package day3;
 public class DemoDay3 {
 	
 	public static void main (String args []) {
+		String country = "USA';
+		String state = "VA";
 		System.out.println("Teresa");
+		
 	}
 
 }
